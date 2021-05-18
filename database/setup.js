81b7b8f01be897ea4f2a8c03badf770de3a2a98d
KolaTolulope <81151811+KolaTolulope@users.noreply.github.com>
2021-05-18 23:43:34 +0100

    Authentication and Authorization fully implemented

--- database/setup.js
+++ database/setup.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 // This will be taken down after grading
-// const DB =
-//   'mongodb+srv://eventDB:QCvd6hO3C84JroeL@cluster0.kia1c.mongodb.net/eventDB?retryWrites=true&w=majority'
+const DB =
+  'mongodb+srv://eventDB:QCvd6hO3C84JroeL@cluster0.kia1c.mongodb.net/eventDB?retryWrites=true&w=majority'
 
-const DB = 'mongodb://localhost:27017/eventDB'
+// const DB = 'mongodb://localhost:27017/eventDB'
 
 module.exports = () => {
   mongoose
